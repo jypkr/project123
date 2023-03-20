a094ffd53d07880bfa3ad89ad3f4c4eb1e94cefb
Wooyul Chung <wooyulc@uci.edu>
2023-03-20 11:46:17 -0700

    2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Cron from './pages/cron/Cron';
+import Signin from './pages/signin/Siginin';
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/cron" element={<Cron/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/cron/Cron.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './cron.css'
+
+const Cron = () => {
+  return (
+    <div>Cron</div>
+  )
+}
+
+export default Cron
